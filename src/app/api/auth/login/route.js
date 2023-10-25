59d6e364f370c3c0b0a02cad43b43613ca8ad26f
Ibrahim Aji <ibrahim.aji@live.com>
2023-10-25 11:23:34 +0700

    fixing authentication

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,22 +1,43 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
 import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 export async function POST(req) {
   const { email, password } = await req.json();
   try {
-    const findUser = await prisma.user.findFirst({
+    const findUser = await prisma.user.findUnique({
       where: {
         email,
       },
     });
-    if (findUser) {
-      const isPasswordMatches = await compare(password, findUser.password);
-      if (isPasswordMatches)
-        return NextResponse.json({ data: findUser }, { status: 200 });
-      return NextResponse.json({ error: "Password salah!" }, { status: 401 });
+    // if (findUser) {
+    //   const isPasswordMatches = await compare(password, findUser.password);
+    //   if (isPasswordMatches)
+    //     return NextResponse.json({ data: findUser }, { status: 200 });
+    //   return NextResponse.json({ error: "Password salah!" }, { status: 401 });
+    // }
+    if (!findUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
     }
-    return NextResponse.json({ data: findUser }, { status: 200 });
+    const matchPassword = await compare(password, findUser.password);
+    if (!matchPassword) {
+      return NextResponse.json(
+        { error: "Invalid email or password" },
+        { status: 400 },
+      );
+    }
+    const payload = {
+      id: findUser.id,
+      name: findUser.name,
+      email: findUser.email,
+    };
+    const accessToken = sign(payload, process.env.JWT_SECRET_KEY,{expiresIn:"10s"});
+    const res = NextResponse.json(
+      { accessToken, data: payload, message: "User login succesfully" }
+    );
+    res.cookies.set("token",accessToken);
+    return res;
   } catch (err) {
     console.log(err);
     return NextResponse.json(err, { status: err.status });
---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import * as jose from "jose";
+
+export default async function middleware(req) {
+  const jwtSecret = process.env.JWT_SECRET_KEY;
+  const encodedJwtSecret = new TextEncoder().encode(jwtSecret);
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  try {
+    const data = await jose.jwtVerify(token, encodedJwtSecret);
+    console.log({ data });
+    return NextResponse.next();
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/","/api/:path*"]
+};
