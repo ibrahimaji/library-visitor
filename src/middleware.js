15fb5860bc9be357621dd9314a68e78de7011bd6
Ibrahim Aji <ibrahim.aji@live.com>
2023-10-26 06:27:33 +0700

    add cors in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,15 +5,21 @@ export default async function middleware(req) {
   const jwtSecret = process.env.JWT_SECRET_KEY;
   const encodedJwtSecret = new TextEncoder().encode(jwtSecret);
   const token = req.cookies.get("token")?.value;
-
-  if (!token) {
+const res = NextResponse.next()
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS)
+    res.headers.append('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN) // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', process.env.ACCESS_CONTROL_ALLOW_METHODS)
+    res.headers.append('Access-Control-Allow-Headers', process.env.ACCESS_CONTROL_ALLOW_HEADERS)
+  
+    if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   try {
     const data = await jose.jwtVerify(token, encodedJwtSecret);
     console.log({ data });
-    return NextResponse.next();
+    return res;
   } catch (error) {
     console.log({ error });
     return NextResponse.redirect(new URL("/login", req.url));
